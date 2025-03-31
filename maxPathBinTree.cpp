c42135c1f02486c4edcef32e7c815efcfc1af078
Wimage03 <nafi314159@gmail.com>
2025-03-31 07:41:13 -0400

    maxPathBinTree

---
+++ maxPathBinTree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solution() {
+    vector<int> denominations = {2, 4, 5};
+    // 7 = 5
+    // 442 = 10
+    // 10 -> 5 
+    // 11 = 5 + 5 + 1
+    // 11 = 2 + 2 + 2 + 5
+    /*
+    5 -> 6
+    5 -> 1
+    5 -> -4 X
+    2 -> -1 X
+    
+    */
+}
+
+int main() {
+    solution();
+}
