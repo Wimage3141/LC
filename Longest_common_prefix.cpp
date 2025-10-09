70d81952eff5f03018b6a53100c2b5205f59b459
Wimage03 <nafi314159@gmail.com>
2025-10-08 23:37:19 -0400

    solved longest common prefix

--- Longest_common_prefix.cpp
+++ Longest_common_prefix.cpp
@@ -11,6 +11,17 @@ public:
             cout << e << " ";
         cout << endl;
     }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        string commonPrefix;
+        int currCharIndex = 0;
+        char currChar = strs[0][0];
+        
+        // Iterate over all chars of the first string
+        for(currCharIndex = 0; currCharIndex < strs[0].size(); currCharIndex++) {
+            currChar = strs[]
+        }
+    }
 };
 
 int main() {
